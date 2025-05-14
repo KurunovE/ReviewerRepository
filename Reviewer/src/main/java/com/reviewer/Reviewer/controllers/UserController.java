aa98f1245c19fb55d7b11bde51c7d84843a6924d
KurunovE <eegor2257@gmail.com>
2025-05-14 23:22:53 +0300

    Change UserController

--- Reviewer/src/main/java/com/reviewer/Reviewer/controllers/UserController.java
+++ Reviewer/src/main/java/com/reviewer/Reviewer/controllers/UserController.java
@@ -1,5 +1,17 @@
 package com.reviewer.Reviewer.controllers;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/api/v1/profile")
+public class UserController {
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId, Model model) {
+        model.addAttribute("user", userId);
+        return "Reviewer-profile";
+    }
 }
