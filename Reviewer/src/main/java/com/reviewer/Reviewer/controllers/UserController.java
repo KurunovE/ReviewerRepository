3411fa4c13bfc8af2ce54e024c557ba3641a8c43
KurunovE <eegor2257@gmail.com>
2025-05-16 13:17:01 +0300

    Add test.html

--- Reviewer/src/main/java/com/reviewer/Reviewer/controllers/UserController.java
+++ Reviewer/src/main/java/com/reviewer/Reviewer/controllers/UserController.java
@@ -1,14 +1,12 @@
 package com.reviewer.Reviewer.controllers;
 
 
-import com.reviewer.Reviewer.models.User.User;
 import com.reviewer.Reviewer.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
