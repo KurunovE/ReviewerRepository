a1a9b6e894bb433377b3d3f0757733c45ccf456e
Gushebush <rodionSokolov06@gmail.com>
2025-05-28 19:21:27 +0300

    updates

--- Reviewer/target/classes/static/js/header-modal-window.js
+++ Reviewer/target/classes/static/js/header-modal-window.js
@@ -5,8 +5,18 @@ const loginMenu = document.querySelector(".login-modal-window__main-content");
 const registerButton = document.querySelector(".login-modal-window__register-button");
 const loginButton = document.querySelector(".login-modal-window__login-button");
 const citiesListBlock = document.querySelector(".select-list ul");
-const geolocationSelectedCity = document.querySelector(".search-input");
+const geolocationSelectedCity = document.querySelector(".geolocation-modal-window__input");
 const geolocationHeaderBlock = document.querySelector(".header__geolocation-block div");
+const loginEmail = document.querySelector(".login-email");
+const loginPassword = document.querySelector(".login-password");
+const registerUsername = document.querySelector(".register-username");
+const registerEmail = document.querySelector(".register-email");
+const registerPassword = document.querySelector(".register-password");
+const registerConfirmPassword = document.querySelector(".register-confirm-password");
+const registerAlertMessage = document.querySelector(".login-modal-window__register-alert-message");
+const registerInputs = [registerUsername,registerEmail,registerPassword,registerConfirmPassword]
+const passwordInputs = [registerPassword,registerConfirmPassword];
+const loginInputs = [loginEmail,loginPassword];
 const cities = [
     "Москва",
     "Санкт-Петербург",
@@ -41,6 +51,7 @@ const cities = [
 ];
 cities.forEach((item)=>{
     let list = document.createElement("li");
+    list.textContent = item;
     list.classList.add("geolocation-list-item");
     citiesListBlock.appendChild(list);
     list.addEventListener("click",()=>{
@@ -65,6 +76,12 @@ document.addEventListener('DOMContentLoaded',() => {
     document.querySelector(".login-modal-window .close-button").addEventListener("click", () => {
         modalBackground.classList.toggle("modal-background-inactive");
         loginModalWindow.classList.toggle("modal-window-inactive");
+        registerInputs.forEach(item =>{
+           item.value = "";
+        });
+        loginInputs.forEach(item =>{
+            item.value = "";
+        })
     });
     document.querySelector(".login-modal-window__login-button").addEventListener("click", () => {
         registerButton.classList.remove("login-button-active");
@@ -77,5 +94,71 @@ document.addEventListener('DOMContentLoaded',() => {
         loginMenu.classList.remove("login-input-active");
         registerButton.classList.add("login-button-active");
         loginMenu.classList.add("register-input-active");
+    });
+    document.querySelector(".login-modal-window__confirm-button").addEventListener("click", ()=>{
+        let registerWindow = document.querySelector(".register-input-active");
+        let loginWindow = document.querySelector(".login-input-active");
+        if (registerWindow){
+            let flag = true;
+            registerInputs.forEach(item => {
+                item.addEventListener("blur",()=>{
+                    if (item.value) item.classList.remove("register-input-inactive");
+                    else item.classList.add("register-input-inactive");
+                })
+                if (!item.value){
+                    flag = false;
+                    item.classList.add("register-input-inactive");
+                }
+            })
+            if (flag){
+                let username = registerUsername.value
+                let email = registerEmail.value;
+                let password = registerPassword.value;
+                let confirmPassword = registerConfirmPassword.value;
+                if (!(email.includes("@"))){
+                    registerAlertMessage.classList.remove("register-alert-message-inactive");
+                    registerAlertMessage.textContent = "Неверный формат email";
+                    registerEmail.classList.add("register-input-inactive");
+                    registerEmail.addEventListener("focus",()=>{
+                        registerEmail.classList.remove("register-input-inactive");
+                    })
+                }
+                else if (password === confirmPassword){
+                    registerAlertMessage.classList.add("register-alert-message-inactive");
+
+                }
+                else{
+                    registerAlertMessage.textContent = "Пароли не совпадают";
+                    registerAlertMessage.classList.remove("register-alert-message-inactive");
+                    passwordInputs.forEach(item => {
+                        item.classList.add("register-input-inactive");
+                        item.addEventListener("focus",()=>{
+                            item.classList.remove("register-input-inactive");
+                        })
+                    })
+
+                }
+            }
+
+        }
+        else if (loginWindow){
+            let flag = true;
+            loginInputs.forEach(item => {
+                item.addEventListener("blur",()=>{
+                    if (item.value) item.classList.remove("register-input-inactive");
+                    else item.classList.add("register-input-inactive");
+                })
+                if (!item.value){
+                    flag = false;
+                    item.classList.add("register-input-inactive");
+                }
+            })
+            if (flag){
+                let email = loginEmail.value;
+                let password = loginPassword.value
+            }
+
+
+        }
     });
 });
